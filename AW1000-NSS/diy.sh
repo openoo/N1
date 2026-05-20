6292f213bcf4047f264790d9a89ee12798804402
openoo <openmekill@gmail.com>
2026-05-21 00:27:59 +0800

    轻量化config

--- AW1000-NSS/diy.sh
+++ AW1000-NSS/diy.sh
@@ -3,13 +3,6 @@
 rm -rf feeds/packages/lang/golang
 git clone --depth=1 --branch=26.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
-# 新版 PassWall 按官方推荐方式引入，先清掉 feeds 里容易冲突的旧核心包。
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf package/passwall-packages package/passwall-luci
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
-
 # 清除 feeds 中自带的 Argon，避免和 jerrykuku 的 master 版本冲突。
 rm -rf feeds/luci/themes/luci-theme-argon feeds/luci/applications/luci-app-argon-config
 git clone --depth=1 --branch=master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
@@ -43,8 +36,8 @@ if [ -f "$sms_tool_makefile" ]; then
   sed -i 's/^PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=skip/' "$sms_tool_makefile"
 fi
 
-# Default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# 默认后台地址
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # NSS 默认走 ECM/NSS 路径，关闭 firewall4 自带 flow offloading。
 # AW1000 的 5G 模组按 qosmio nss-setup 文档预置 3 个接口：
